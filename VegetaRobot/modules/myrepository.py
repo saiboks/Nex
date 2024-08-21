184be096280563847cb4882c51bcb3ba83b17798
saiboks <botssai69@gmail.com>
2024-08-21 19:04:46 +0530

    Update myrepository.py

--- VegetaRobot/modules/myrepository.py
+++ VegetaRobot/modules/myrepository.py
@@ -9,20 +9,34 @@ from pyrogram.types import *
 
 
 @pgram.on_message(filters.command("repo"))
-async def repo(_, m):
-    chat_id = m.chat.id
-    users = requests.get("https://api.github.com/repos/NandhaxD/VegetaRobot/contributors").json()
-    list_of_users = ""
-    count = 1
-    for user in users:
-        list_of_users += (f"**{count}.** [{user['login']}]({user['html_url']})\n")
-        count += 1
-        total = count-1
-    text = f"""
-[ Contributors in @VegetaRobot ]
+async def repo(client, message):
+    await message.reply_photo(
+        photo=AVISHA,
+        caption=f"""𖣐 ʜᴇʏ {message.from_user.mention()}, ᴡᴇʟᴄᴏᴍᴇ ʙᴀʙʏ ♥︎\n\n⬤ ɪғ ʏᴏᴜ ᴡᴀɴᴛ [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username}) ʙᴏᴛ ʀᴇᴘᴏ, ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʀᴇᴘᴏ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ᴍʏ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ.\n\n𖣐 ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username}) """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "ᴏᴡɴᴇʀ",user_id=OWNER_ID
+                    ),
+                    InlineKeyboardButton(
+                        "ʀᴇᴘᴏ",
+                        callback_data="gib_source",
+                    ),
+                ]
+            ]
+        ),
+    )
 
-{list_of_users}
-[`Contributors: {total}`]"""
-    await pgram.send_message(chat_id,text=text,
-    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Rᴇᴘᴏ",url="https://gitHub.com/NandhaxD/VegetaRobot"),
-InlineKeyboardButton("𝐆ʀᴏᴜᴘ",url="t.me/PhoenixXsupport"),]]) ,reply_to_message_id=m.id ,disable_web_page_preview=True)
+
+@app.on_callback_query(filters.regex("gib_source"))
+async def gib_repo_callback(_, callback_query):
+    await callback_query.edit_message_media(
+        media=InputMediaVideo("https://telegra.ph/file/9235d57807362b4e227a3.mp4", has_spoiler=True),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [close_button]
+            ]
+        ),
+        )
+close_button = InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")
